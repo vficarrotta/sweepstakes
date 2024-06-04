fffe3c568da0ddd5177fbee46268e326d49235ae
Vince Ficarrotta <vficarrotta@gmail.com>
2024-06-04 12:19:31 -0500

    Update adam_lz_sweep_ends-6-24-2024.r

--- adam_lz_sweep_ends-6-24-2024.r
+++ adam_lz_sweep_ends-6-24-2024.r
@@ -12,9 +12,9 @@ form <- 'https://docs.google.com/forms/d/e/1FAIpQLSd6piC0i91U57QA2_abXlRz7uRhTL5
 ## start the server
 rD <- rsDriver(browser="firefox", # version is latest as of 5/23/2024
                 port=free_port(), # free_port() from the netstat package
-                chromever=NULL,   # explicit no chrome argument
-                version='latest', # explicit the latest
-                verbose=F)        # indoor voice, computer
+                chromever=NULL,   # explicate no chrome 
+                version='latest', # explicate the latest
+                verbose=F)        # indoor voice
 
 ## client object
 remDr <- rD$client
@@ -25,8 +25,12 @@ remDr$open()
 ## go to url
 remDr$navigate(form)
 
-Sys.sleep(3) # give page time to load before getting spammed
-
+## variables
+email <- 'vficarrotta@gmail.com'
+fn <- 'vince'
+ln <- 'ficarrotta'
+pn <- '9047350138'
+address <- '3297 S College St, apt D204, Auburn, Alabama 36830'
 
 #### Filling the form
 
@@ -34,32 +38,32 @@ for(i in 1:1000000){
 
     remDr$navigate(form)
 
-    Sys.sleep(3) # give page time to load before getting spammed
+    Sys.sleep(1.5) # give page time to load before getting spammed
 
     ## email
     entering <- remDr$findElement(using='xpath', '//*[contains(concat( " ", @class, " " ), concat( " ", "oQYVNd", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "zHQkBf", " " ))]')
     entering$clickElement()
-    entering$sendKeysToElement(list('vficarrotta@gmail.com', key='enter'))
+    entering$sendKeysToElement(list(email, key='enter'))
 
     ## first name
     entering <- remDr$findElement(using='xpath', '//*[contains(concat( " ", @class, " " ), concat( " ", "Qr7Oae", " " )) and (((count(preceding-sibling::*) + 1) = 2) and parent::*)]//*[contains(concat( " ", @class, " " ), concat( " ", "zHQkBf", " " ))]')
     entering$clickElement()
-    entering$sendKeysToElement(list('vince', key='enter'))
+    entering$sendKeysToElement(list(fn, key='enter'))
 
     ## last name
     entering <- remDr$findElement(using='xpath', '//*[contains(concat( " ", @class, " " ), concat( " ", "Qr7Oae", " " )) and (((count(preceding-sibling::*) + 1) = 3) and parent::*)]//*[contains(concat( " ", @class, " " ), concat( " ", "zHQkBf", " " ))]')
     entering$clickElement()
-    entering$sendKeysToElement(list('ficarrotta', key='enter'))
+    entering$sendKeysToElement(list(ln, key='enter'))
 
     ## address
     entering <- remDr$findElement(using='xpath', '//*[contains(concat( " ", @class, " " ), concat( " ", "tL9Q4c", " " ))]')
     entering$clickElement()
-    entering$sendKeysToElement(list('3297 S College St, apt D204, Auburn, Alabama 36830', key='enter'))
+    entering$sendKeysToElement(list(address, key='enter'))
 
     ## phone number
     entering <- remDr$findElement(using='css selector', '.Qr7Oae~ .Qr7Oae+ .Qr7Oae .zHQkBf')
     entering$clickElement()
-    entering$sendKeysToElement(list('9047350138', key='enter'))
+    entering$sendKeysToElement(list(pn, key='enter'))
 
     ## how did you hear about us?
     entering <- remDr$findElement(using='css selector', '.Id5V1')
